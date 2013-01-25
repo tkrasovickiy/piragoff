c7dfc243f4b38088faa77671b9f321b4e80968e8
Tikhon Krasovitskiy <tik714@yandex.ru>
2013-01-25 22:00:10 +0300

    create new application for my project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Piragoff::Application.config.secret_token = 'cd1b5e71564a1e95667bdf0f693f43440c5d6401e883042363b6077f9e1e4db28601684ae9dec3130dbd82751cc43a713002dfe93b27538144b73a99b4f78cf8'
